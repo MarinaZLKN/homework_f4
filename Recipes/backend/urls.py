c1ba2e64ba232ab797afe10b8965b9a8a4a7dc60
Marina <marina.zilkina@gmail.com>
2023-03-20 14:11:36 +0200

    different paths for apis ready

--- Recipes/backend/urls.py
+++ Recipes/backend/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 
-from backend.views import RecipeAPIView, CategoryAPIView
+from backend.views import RecipeAPIView, CategoryAPIView, RecipeDetailView, CategoryDetailView
 
 urlpatterns = [
-    path('recipelist/', RecipeAPIView.as_view()),
-    path('categorylist/', CategoryAPIView.as_view()),
+    path('recipelist/', RecipeAPIView.as_view({'get': 'list'})),
+    path('categorylist/', CategoryAPIView.as_view({'get': 'list'})),
+    path('recipelist/<int:pk>/', RecipeDetailView.as_view({'get': 'retrieve'})),
+    path("categorylist/<int:pk>/", CategoryDetailView.as_view({"get": "list"})),
 ]
--- Recipes/backend/views.py
+++ Recipes/backend/views.py
@@ -1,16 +1,31 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Recipe, Category
 from .serializers import RecipeSerializer, CategorySerializer
 
 
-class RecipeAPIView(generics.ListAPIView):
+class RecipeAPIView(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
 
 
-class CategoryAPIView(generics.ListAPIView):
+class CategoryAPIView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class CategoryDetailView(viewsets.ModelViewSet):
+
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        recipes = Recipe.objects.filter(category=self.kwargs["pk"])
+        return recipes
+
+
+class RecipeDetailView(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
 
 
 
